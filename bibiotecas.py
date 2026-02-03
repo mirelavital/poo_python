86a7643336414740baa68db74d89daa8bb2e8224
mirelavital <153030670+mirelavital@users.noreply.github.com>
2026-02-03 16:18:44 -0300

    Biblioteca de arquivos

---
+++ bibiotecas.py
@@ -0,0 +1,16 @@
+import math
+
+
+n = float(input("Digite um número: "))
+raiz = math.sqrt(n)
+print(f"A raiz de {n} é : {math.ceil(raiz):.2f}")
+
+print("-" *40)
+
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+resultado = math.pow(base,expoente)
+print(f"A {base} elevado a {expoente} = {resultado}")
+
+
+
